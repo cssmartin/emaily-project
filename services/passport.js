039b3e2334ee37e1bf4674ebf40bb41f3e4d731e
cssmartin <chelseasmartin@icloud.com>
2023-11-28 11:31:02 +0000

    fixed proxy variable

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleclientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
