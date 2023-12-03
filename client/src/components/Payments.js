f9238190cbfb7e13d922eea722792e67586bccca
cssmartin <chelseasmartin@icloud.com>
2023-12-03 20:07:43 +0000

    fixing versions  stripe

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import StripeCheckout from "react-stripe-checkout";
+import StripeCheckout from "@stripe/react-stripe.js";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
